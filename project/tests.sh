83d9f061f9ebe5b4c3c243e4e116f87da8e089c3
Elva Mo <elva.lk.mo@fau.de>
2023-12-19 17:00:32 +0100

    update ci

--- project/tests.sh
+++ project/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install --upgrade pip
-pip install -r /project/prequirements.txt
+pip install -r project/prequirements.txt
 
 python3 project/pipeline.py
 python3 project/test_output.py
